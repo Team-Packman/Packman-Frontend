e6c2356c73e246972dc4478ec4a212a06ebac497
n0eyes <doll4586633@naver.com>
2023-11-01 20:27:03 +0900

    feat: utility type 추가
    - CamelToSnake
    - SnakeToCamel

---
+++ src/types/@common/utility.ts
@@ -0,0 +1,33 @@
+type Separator = '_';
+
+type IsChar<Char extends string> = Uppercase<Char> extends Lowercase<Char> ? false : true;
+
+type IsCapitalized<Char extends string> = IsChar<Char> extends true
+  ? Uppercase<Char> extends Char
+    ? true
+    : false
+  : false;
+
+type isLowercase<T extends string> = T extends Lowercase<T> ? true : false;
+
+type ReplaceCapitalToSeparator<Char extends string> = IsCapitalized<Char> extends true
+  ? `${Separator}${Lowercase<Char>}`
+  : Char;
+
+type CamelToSnake<
+  Str extends string,
+  Acc extends string = '',
+> = Str extends `${infer Char}${infer Rest}`
+  ? CamelToSnake<Rest, `${Acc}${ReplaceCapitalToSeparator<Char>}`>
+  : Acc;
+
+type SnakeToCamel<
+  Str extends string,
+  Acc extends string = '',
+> = Str extends `${infer Char}${infer Rest}`
+  ? Str extends `${Separator}${infer Char}${infer Rest}`
+    ? SnakeToCamel<Rest, `${Acc}${Uppercase<Char>}`>
+    : SnakeToCamel<Rest, `${Acc}${Lowercase<Char>}`>
+  : Acc;
+
+export type { CamelToSnake, isLowercase, SnakeToCamel };
