ca493d0deb79aa26ecdc19cce7f3a52775fb8b24
n0eyes <doll4586633@naver.com>
2023-11-01 11:31:11 +0900

    chore: css-loader, style-loader 적용

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -51,6 +51,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
