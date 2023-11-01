23237367066a2c8d8e64964f287f63283d58004f
n0eyes <doll4586633@naver.com>
2023-11-01 20:38:13 +0900

    feat: QueryClientProvider 추가 및 defaultOptions 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-const App = () => <div>Hello Packman</div>;
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { ONE_HOUR } from './constants/time';
+import { Stack } from './router/stackflow';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: false,
+      throwOnError: true,
+      staleTime: ONE_HOUR,
+      gcTime: ONE_HOUR,
+    },
+  },
+});
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Stack />
+  </QueryClientProvider>
+);
 
 export default App;
