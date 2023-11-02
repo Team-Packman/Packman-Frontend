0c545811cb10ce943d783f017a433a110eba7dbb
n0eyes <doll4586633@naver.com>
2023-11-02 16:52:27 +0900

    feat: safari 툴 바 영역을 제외한 공간을 스크린 높이로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './components/@common/GlobalStyle';
 import { ONE_HOUR } from './constants/time';
 import { Stack } from './router/stackflow';
 import theme from './styles/theme/theme';
+import { setScreenSize } from './utils/setScreenSize';
+
+setScreenSize();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,9 +26,26 @@ const App = () => (
   <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
+      <GlobalEvent />
       <Stack />
     </QueryClientProvider>
   </ThemeProvider>
 );
 
 export default App;
+
+const GlobalEvent = () => {
+  useEffect(() => {
+    const onResize = () => {
+      setScreenSize();
+    };
+
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return null;
+};
