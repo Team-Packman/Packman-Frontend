3762cefe5307e7b11b7a6e081d279554e3810798
n0eyes <doll4586633@naver.com>
2023-11-04 22:15:26 +0900

    refactor: AppLayout height 수정

--- src/components/@common/AppLayout/AppLayout.styles.tsx
+++ src/components/@common/AppLayout/AppLayout.styles.tsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Layout = styled.div`
   max-width: 48rem;
-  min-height: 100vh;
+  min-height: calc((var(--vh, 1vh) * 100));
   margin: 0 auto;
 `;
