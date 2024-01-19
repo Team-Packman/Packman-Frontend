1792a0679fbc54c486507df2732593052f69714d
n0eyes <doll4586633@naver.com>
2024-01-19 21:46:31 +0900

    refactor: storybook 전역 레이아웃 수정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,8 +5,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from '../src/styles/theme/theme';
 import { ONE_HOUR } from '@/constants/time';
 import globalStyle from '@/styles/globalStyle';
-import AppLayout from '@/components/@common/AppLayout/AppLayout';
-import AppScreen from '@/components/@common/AppScreen/AppScreen';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,20 +38,16 @@ const preview: Preview = {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <Global styles={globalStyle} />
-          <AppLayout>
-            <AppScreen appBar={{ left: null, title: '', right: null }}>
-              <div
-                css={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '1rem',
-                  width: '100%',
-                }}
-              >
-                <Story />
-              </div>
-            </AppScreen>
-          </AppLayout>
+          <div
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem',
+              width: '48rem',
+            }}
+          >
+            <Story />
+          </div>
         </ThemeProvider>
       </QueryClientProvider>
     ),
