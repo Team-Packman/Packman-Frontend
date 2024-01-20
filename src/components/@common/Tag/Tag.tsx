821ca405c6f312f0a503426982632674ca61a4e0
n0eyes <doll4586633@naver.com>
2024-01-21 01:01:58 +0900

    refactor: Tag 컴포넌트 스타일 통합

--- src/components/@common/Tag/Tag.tsx
+++ src/components/@common/Tag/Tag.tsx
@@ -1,9 +1,18 @@
-import { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
-
-import * as Styled from './Tag.styles';
+import styled from '@emotion/styled';
+import type { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
 
 type TagProps = ComponentPropsWithoutRef<'span'>;
 
-const Tag = (props: PropsWithChildren<TagProps>) => <Styled.Layout {...props} />;
+const Layout = styled.span`
+  width: fit-content;
+  padding: 0.2rem 0.8rem;
+
+  background-color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.pmPink};
+  ${({ theme }) => theme.typo.body1}
+  border-radius: 12px;
+`;
+
+const Tag = (props: PropsWithChildren<TagProps>) => <Layout {...props} />;
 
 export default Tag;
